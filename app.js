0338a1216b90b6e48adbba5cb189384573827818
Abdibasidissack <45002583+Abdibasidissack@users.noreply.github.com>
2020-01-21 19:23:36 -1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buying food","Cooking food","Eating food"];
+let workItems = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+
+    let today = new Date();
+    let options = {
+        weekday : 'long',
+        day: 'numeric',
+        month:'long'
+    };
+    let day = today.toLocaleDateString("en-US",options);
+
+    
+
+    res.render("list", { listTitle: day, newListItems   : items});
+});
+
+app.post("/", (req,res)=>{
+    let item = req.body.newItem;
+
+    if(req.body.list === "work")
+    {
+        workItems.push(item);
+        res.redirect("/work")
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+    
+    
+});
+
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Work List", newListItems:workItems})
+});
+app.post("/work", (req,res)=>{
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+});
+
+app.listen(3000, () => console.log("we are good to start the todo list web app"));
